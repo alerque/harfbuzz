faaa296b47fb86ed7cb78ffe63944f27432344b4
Caleb Maclennan <caleb@alerque.com>
2020-09-11 18:01:51 +0300

    [TMP] Debug path

--- .ci/publish_release_artifact.sh
+++ .ci/publish_release_artifact.sh
@@ -13,6 +13,11 @@ export _GHR=ghr_${_GHR_VER}_linux_amd64
 curl -sfL https://github.com/tcnksm/ghr/releases/download/$_GHR_VER/$_GHR.tar.gz |
 	tar xz -C $HOME/.local/bin --strip-components=1 $_GHR/ghr
 
+echo $PATH
+env
+ls -al $HOME/.local/bin
+which ghr
+
 ghr -replace \
 	-u $CIRCLE_PROJECT_USERNAME \
 	-r $CIRCLE_PROJECT_REPONAME \
